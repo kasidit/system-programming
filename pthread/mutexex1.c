f52b6fa2d36a90bdc02198e228ffa954cff562b5
kasidit chanchio <kasiditchanchio@gmail.com>
2020-04-02 14:58:26 +0700

    update

--- pthread/mutexex1.c
+++ pthread/mutexex1.c
@@ -18,18 +18,22 @@ void * foo(void * arg){
 
 int main(void){
 
-   pthread_t mtid, ntid;
+   pthread_t mtid, ntid[3];
    void *tret; 
     
    pthread_mutex_init(&mutex_var, NULL); 
 
-   pthread_create(&ntid, NULL, foo, NULL);  
+   pthread_create(&ntid[0], NULL, foo, NULL);  
+   pthread_create(&ntid[1], NULL, foo, NULL);  
+   pthread_create(&ntid[2], NULL, foo, NULL);  
 
    pthread_mutex_lock(&mutex_var);  
    global_var += 75;
    printf("main() update global_var to %d\n", global_var); 
    pthread_mutex_unlock(&mutex_var);  
 
-   pthread_join(ntid, tret); 
+   pthread_join(ntid[0], tret); 
+   pthread_join(ntid[1], tret); 
+   pthread_join(ntid[2], tret); 
 
 }

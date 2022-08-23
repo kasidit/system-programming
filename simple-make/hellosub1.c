657e208ca5be1d085b973598c7d508c75a98bad2
kasidit chanchio <kasiditchanchio@gmail.com>
2022-08-23 22:25:23 +0700

    del

--- simple-make/hellosub1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "hello1.h"
-
-void fun1(void){
-  printf("hello1 %d\n", CONSTANT1);
-}
--- simple-make/hellosub2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void fun2(void){
-  printf("hello2\n");
-}
-
